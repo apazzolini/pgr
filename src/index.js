0cccca901e53ce0ebe73e90ada8b76855ac8cde6
Andre Azzolini <apazzolini@gmail.com>
2018-05-18 16:11:16 -0500

    Expose pg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const pg = require('pg')
 const { createPool, getPool } = require('./pool.js')
 const sql = require('./sql.js')
 const query = require('./query.js')
 
 module.exports = {
+    pg,
     sql,
     query,
     createPool,
